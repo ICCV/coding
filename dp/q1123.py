cf24d83b60b5b518fdea491da27f4295394f5099
yufan <yufan@yufandeMacBook-Pro.local>
2021-08-03 18:22:10 +0800

    update

--- dp/q1123.py
+++ dp/q1123.py
@@ -5,3 +5,18 @@ class Solution(object):
         :type text2: str
         :rtype: int
         """
+        m,n = len(text1),len(text2)
+        status = [[0]*(n+1) for _ in range(m+1)]
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if text1[i-1] == text2[j-1]:
+                    status[i][j] = status[i-1][j-1] +1
+                else:
+                    status[i][j] = max(status[i-1][j-1],status[i-1][j],status[i][j-1])
+        return status[m][n]
+
+if __name__ == '__main__':
+    s = Solution()
+    text1 = "abcde"
+    text2 = "ace"
+    print(s.longestCommonSubsequence(text1,text2))
